b2f29f7e40a676b728bb7083a418aa93c709e3d2
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-01-16 14:40:37 +0700

    .commit

--- Dashboard.py
+++ Dashboard.py
@@ -1,20 +1,12 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-
-# กำหนดข้อมูล
-df = pd.DataFrame({
-    "ปี": [2022, 2023, 2024],
-    "ยอดขาย": [100, 200, 300],
-    "ภูมิภาค": ["เอเชีย", "ยุโรป", "อเมริกา"]
-})
 
 # สร้างแดชบอร์ด
-st.title("ยอดขายประจำปี")
+st.title("ทดสอบการใช้งาน Streamlit")
 
 st.header("Header")
 st.subheader('Raw data')
 
-# แสดงข้อมูลในตาราง
-st.write(df.head(10))
+# แสดงความ
+st.write("testing")
 
