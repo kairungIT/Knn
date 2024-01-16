01c624c8b88097ab021469be583bedbd6306caa3
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-01-16 15:57:38 +0700

    .commit

--- Dashboard.py
+++ Dashboard.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import datetime
+import torch
 
 # สร้างแดชบอร์ด
 st.title("ทดสอบการใช้งาน Streamlit")
@@ -77,3 +79,36 @@ with col2:
 
     if text_input:
         st.write("You entered: ", text_input)
+
+number = st.number_input('Insert a number')
+st.write('The current number is ', number)
+
+d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
+st.write('Your birthday is:', d)
+
+t = st.time_input('Set an alarm for', datetime.time(8, 45))
+st.write('Alarm is set for', t)
+
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
+
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+    # To read image file buffer as a 3D uint8 tensor with PyTorch:
+    bytes_data = img_file_buffer.getvalue()
+    torch_img = torch.ops.image.decode_image(
+        torch.from_numpy(np.frombuffer(bytes_data, np.uint8)), 3
+    )
+
+    # Check the type of torch_img:
+    # Should output: <class 'torch.Tensor'>
+    st.write(type(torch_img))
+
+    # Check the shape of torch_img:
+    # Should output shape: torch.Size([channels, height, width])
+    st.write(torch_img.shape)
